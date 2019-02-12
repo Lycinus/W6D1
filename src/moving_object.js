c1206a240123b624bf3bcad5ce40c213ea111d7c
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2019-02-11 17:30:05 -0800

    Add Asteroids project

---
+++ src/moving_object.js
@@ -0,0 +1,13 @@
+function MovingObject(options) {
+ this.pos = option[pos];
+ this.vel = option[vel];
+ this.radius = option[radius];
+ this.color = option[color];
+
+}
+
+MovingObject.prototype.draw(ctx) = function() {
+
+}
+
+module.exports = MovingObject;
